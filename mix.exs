0e6777281219e08baa4a712c4a2e41c290d591a9
Mafinar Khan <mafinar@gmail.com>
2020-01-19 23:56:22 -0500

    Update Elixir Version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Algo.MixProject do
     [
       app: :elixir_algorithms,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
